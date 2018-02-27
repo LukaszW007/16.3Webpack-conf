1c7f050979206d47c3903b546bbe2b3572770c56
LukaszWiszniewski <wisznu07@gmail.com>
2018-02-27 14:47:13 +0100

    Configured Webpack

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -67,13 +67,18 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
+"use strict";
+
+
 var log = __webpack_require__(1);
 log('hello world');
 
-
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
 
 module.exports = console.log;
 
